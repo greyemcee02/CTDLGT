609d7d1b9d332fa653f0d15bde44276746adfaf7
cuibapest.15F70C2 <dluffy2332@gmail.com>
2022-05-30 20:05:24 +0700

    fix

--- DSA11014.cpp
+++ DSA11014.cpp
@@ -10,67 +10,54 @@ struct Node{
 		right = NULL;
 	}
 };
-int n;
-vector<Node *> par;
+int n, res;
+map<int, Node *> mp;
+
 void makeNode(Node *root, int b, char c){
-	if(c == 'L' && root->left == NULL){
+	if(c == 'L' ){
 		root->left = new Node(b);
-	}	
-	else if(c == 'R' && root->right == NULL){
+		mp[b] = root->left;
+	}
+	else if(c == 'R' ){
 		root->right = new Node(b);
-	}	
+		mp[b] = root->right;
+	}		
 }
 
-void find(Node *root, Node *p, int a, int b, char c){
+void insert(Node *root, int b, char c){
+	if(root != NULL){
+		makeNode(root, b, c);
+	}
+}
+
+void travel(Node *root, int ok){
 	if(root == NULL) return;
-	if(root->data == a && root->right == NULL){
-		p = root;
-	}
-	insert(root->left, a, b, c);
-	insert(root->right, a, b, c);
-}
-
-
-void rightLeafNode(Node *root){
-	queue<Node *> Q;
-	Q.push(root);
-	int res = 0;
-	while(!Q.empty()){
-		Node *p = Q.front();
-		Q.pop();
-		if(p->left != NULL) Q.push(p->left);
-		if(p->right != NULL){
-			if(p->right->left!= NULL || p->right->right!= NULL){
-				Q.push(p->right);
-			}
-			else res += p->right->data;
-		}
-	}
-	cout << res << endl;
+	else if(root->left == NULL && root->right == NULL){
+		if(ok) res += root->data;
+		return;
+	} 
+	travel(root->left, 0);
+	travel(root->right, 1);
 }
 
 void solve(){
+	mp.clear();
 	cin >> n;
 	int a, b;
 	char c;
-	par.resize(10001, NULL);
+	res = 0;
 	Node *root = NULL;
 	for(int i = 1; i <= n; i++){
 		cin >> a >> b >> c;
 		if(root == NULL){
 			root = new Node(a);
-			par[a] = root;
+			mp[a] = root;
 			makeNode(root, b, c);
 		}
-		else{
-			Node *p = NULL;
-			find(root, p, a, b, c);
-			if(p != NULL){
-				
-			}
-		} 
+		else insert(mp[a], b, c);
 	}
-	rightLeafNode(root);
+	travel(root, 0);
+	cout << res << endl;
 }
 
 int main() {
